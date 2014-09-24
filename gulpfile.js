8081e8353bbad1ea42a471ee76092f97f31005ab
Harmen Janssen <harmen@grrr.nl>
2014-09-24 13:47:20 +0200

    Build minified and full versions

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
  
 gulp.task('default', function() {
+	// Full version
 	gulp.src(['src/**/module.js', 'src/**/*.js'])
 		.pipe(concat('grrr-scroll.js'))
+		.pipe(gulp.dest('dist/'));
+	gulp.src(['src/**/module.js', 'src/**/*.js'])
+		.pipe(concat('grrr-scroll.min.js'))
 		.pipe(ngAnnotate())
 		.pipe(uglify())
-		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist/'));
 });
